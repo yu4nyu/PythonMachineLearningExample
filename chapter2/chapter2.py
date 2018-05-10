703ace16f9859252a6b8c1dcf72732ba234c6613
YUAN Yu <yuan@archos.com>
2018-05-10 17:18:01 +0800

    better chapter2 scripts

--- chapter2/chapter2.py
+++ chapter2/chapter2.py
@@ -23,9 +23,9 @@ plt.show()
 
 
 ### 003 train perceptron
-import perceptron as ppn_
+from perceptron import Perceptron
 
-ppn = ppn_.Perceptron(eta=0.1, n_iter=10)
+ppn = Perceptron(eta=0.1, n_iter=10)
 ppn.fit(X, y)
 plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
 plt.xlabel('Epochs')
@@ -35,9 +35,9 @@ plt.show()
 
 
 ### 004 plot decision boundary
-import plot_decision_regions as pdr_
+from plot_decision_regions import plot_decision_regions
 
-pdr_.plot_decision_regions(X, y, classifier=ppn)
+plot_decision_regions(X, y, classifier=ppn)
 plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc='upper left')
@@ -46,15 +46,15 @@ plt.show()
 
 
 ### 005 plot the cost against the number of epochs for the two different learning rates
-import adaline_gd as ag_
+from adaline_gd import AdalineGD
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
-ada1 = ag_.AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
 ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
 ax[0].set_xlabel('Epochs')
 ax[0].set_ylabel('log(Sum-squared-error)')
 ax[0].set_title('Adaline - Learning rate 0.01')
-ada2 = ag_.AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
 ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
 ax[1].set_xlabel('Epochs')
 ax[1].set_ylabel('Sum-squared-error')
@@ -73,9 +73,9 @@ X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
 
 ### 007 train the Adaline again and see that it now converges using a learning rate 0.01
 
-ada = ag_.AdalineGD(n_iter=15, eta=0.01)
+ada = AdalineGD(n_iter=15, eta=0.01)
 ada.fit(X_std, y)
-pdr_.plot_decision_regions(X_std, y, classifier=ada)
+plot_decision_regions(X_std, y, classifier=ada)
 plt.title('Adaline - Gradient Descent')
 plt.xlabel('sepal length [standardized]')
 plt.ylabel('petal length [standardized]')
@@ -89,11 +89,11 @@ plt.show()
 
 
 # 008 train the AdalineSGD
-import adaline_sgd as as_
+from adaline_sgd import AdalineSGD
 
-ada = as_.AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
 ada.fit(X_std, y)
-pdr_.plot_decision_regions(X_std, y, classifier=ada)
+plot_decision_regions(X_std, y, classifier=ada)
 plt.title('Adaline - Stochastic Gradient Descent')
 plt.xlabel('sepal length [standardized]')
 plt.ylabel('petal length [standardized]')
--- chapter2/plot_decision_regions.py
+++ chapter2/plot_decision_regions.py
@@ -10,8 +10,8 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
     # plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() +1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() +1
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
