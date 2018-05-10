e0eca837ea11e59e805d8a2f672057567055e59a
YUAN Yu <yuan@archos.com>
2018-05-10 18:33:12 +0800

    update chapter3 scripts

--- chapter3/chapter3.py
+++ chapter3/chapter3.py
@@ -46,3 +46,73 @@ print('Misclassified samples: %d' % (y_test != y_pred).sum())
 from sklearn.metrics import accuracy_score
 
 print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+
+
+### 007 plot decision regions
+from plot_decision_regions import plot_decision_regions
+import matplotlib.pyplot as plt
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+
+
+### 008 plot the sigmoid function
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+z = np.arange(-7, 7, 0.1)
+phi_z = sigmoid(z)
+plt.plot(z, phi_z)
+plt.axvline(0.0, color='k')
+plt.axhspan(0.0, 1.0, facecolor='1.0', edgecolor='0.0', alpha=1.0, ls='dotted')
+plt.axhline(y=0.5, ls='dotted', color='k')
+plt.yticks([0.0, 0.5, 1.0])
+plt.ylim(-0.1, 1.1)
+plt.xlabel('z')
+plt.ylabel('$\phi (z)$')
+plt.show()
+
+
+
+### 009 train a logistic regression model
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression(C=1000.0, random_state=0)
+lr.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+
+
+# 010 predict the probabilities of one sample
+print(lr.predict_proba(X_test_std[0,:].reshape(1, -1)))
+
+
+
+# 011 demonstrate effect of parameter C
+weights, params = [], []
+for c in np.arange(-5, 5):
+    param = 10**float(c)
+    lr = LogisticRegression(C=param, random_state=0)
+    lr.fit(X_train_std, y_train)
+    # We only collect the weight coefficients of the class 2 vs. all classifer.
+    # Remember that we are using the OvR techniqu for multiclass classification.
+    weights.append(lr.coef_[1])
+    params.append(param)
+weights = np.array(weights)
+plt.plot(params, weights[:, 0], label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--', label='petal width')
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+plt.xscale('log')
+plt.show()
