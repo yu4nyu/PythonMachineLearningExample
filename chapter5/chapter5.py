9d1d66c94362ff59c2480555825089c539e6cf75
YUAN Yu <yuan@archos.com>
2018-05-17 18:36:56 +0800

    add chapter5 script

---
+++ chapter5/chapter5.py
@@ -0,0 +1,37 @@
+### 001 prepare data
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.fit_transform(X_test)
+
+
+
+### 002 obtain the eigenpairs
+import numpy as np
+
+cov_mat = np.cov(X_train_std.T) # covariance matrix
+eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
+print('\nEigenvalues \n%s' % eigen_vals)
+print()
+
+
+
+### 003 explained variances
+import matplotlib.pyplot as plt
+
+tot = sum(eigen_vals)
+var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
+cum_var_exp = np.cumsum(var_exp) # cumulative sum
+
+plt.bar(range(1,14), var_exp, alpha=0.5, align='center', label='individual explained variance')
+plt.step(range(1,14), cum_var_exp, where='mid', label='cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal components')
+plt.legend(loc='best')
+plt.show()
