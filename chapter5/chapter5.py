d7ff72ee5ae76073838f08adfdc2ac99d38c7a35
YUAN Yu <yu-l116@rouchu.com>
2018-05-20 18:25:42 +0800

    update chapter5 scripts

--- chapter5/chapter5.py
+++ chapter5/chapter5.py
@@ -36,3 +36,238 @@ plt.ylabel('Explained variance ratio')
 plt.xlabel('Principal components')
 plt.legend(loc='best')
 plt.show()
+
+
+
+### 004 Feature transformation
+print('### 004')
+# sort the eigenpairs by decreasing order
+eigen_pairs = [(np.abs(eigen_vals[i]),eigen_vecs[:,i]) for i in range(len(eigen_vals))]
+eigen_pairs.sort(reverse=True)
+# get two eigenvectors that correspond to the two largest values
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:, np.newaxis]))
+print('Matrix W:\n',w)
+# transform a sample x (1x13) onto the PCA subspace btaining x'
+print(X_train_std[0].dot(w))
+print()
+# transform the entire training dataset
+X_train_pca = X_train_std.dot(w)
+# visualize
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+for l, c, m in zip(np.unique(y_train), colors, markers):
+	plt.scatter(X_train_pca[y_train==l, 0], X_train_pca[y_train==l, 1], c=c, label=l, marker=m)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.show()
+# Although we encoded the class labels information for the purpose of illustration in
+# the preceding scatter plot, we have to keep in mind that PCA is an unsupervised
+# technique that doesn't use class label information.
+
+
+
+### 005 use PCA from scikit-learn
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from plot_decision_regions import plot_decision_regions
+
+pca = PCA(n_components=2)
+lr = LogisticRegression()
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+lr.fit(X_train_pca, y_train)
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC1')
+plt.xlabel('PC2')
+plt.legend(loc='lower left')
+plt.show()
+
+# plot the test data
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.show()
+
+
+
+### 006 n_compnetns st to None to access explained_variance_ratio_
+print('### 006')
+pca = PCA(n_components=None)
+X_train_pca = pca.fit_transform(X_train_std)
+print(pca.explained_variance_ratio_)
+print()
+# Note that we set n_components=None when we initialized the PCA class so that
+# it would return all principal components in sorted order instead of performing a
+# dimensionality reduction.
+
+
+
+### 007 mean vector
+print('### 007')
+np.set_printoptions(precision=4)
+mean_vecs = []
+for label in range(1,4):
+	mean_vecs.append(np.mean(X_train_std[y_train==label], axis=0))
+	print('MV %s: %s\n' %(label, mean_vecs[label-1]))
+
+
+
+### 008 within-class scatter matrix
+print('### 008')
+d = 13 # number of features
+S_W = np.zeros((d, d))
+for label,mv in zip(range(1,4), mean_vecs):
+	class_scatter = np.zeros((d, d))
+	for row in X[y == label]:
+		row, mv = row.reshape(d, 1), mv.reshape(d, 1)
+		class_scatter += (row-mv).dot((row-mv).T)
+	S_W += class_scatter
+print('Within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
+print()
+
+
+
+### 009 scaled with-class scatter matrix
+print('### 009')
+print('Class label distribution %s' % np.bincount(y_train)[1:])
+
+d = 13 # number of features
+S_W = np.zeros((d, d))
+for label,mv in zip(range(1,4), mean_vecs):
+	class_scatter = np.cov(X_train_std[y_train==label].T)
+	S_W += class_scatter
+print('Scaled within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
+print()
+
+
+
+### 010 between-class scatter matrix
+print('### 010')
+mean_overall = np.mean(X_train_std, axis=0)
+d = 13 # number of features
+S_B = np.zeros((d, d))
+for i,mean_vec in enumerate(mean_vecs):
+	n = X[y==i+1, :].shape[0]
+	mean_vec = mean_vec.reshape(d, 1)
+	mean_overall = mean_overall.reshape(d, 1)
+	S_B += n * (mean_vec - mean_overall).dot((mean_vec - mean_overall).T)
+print('Between-class scatter matrix: %sx%s' % (S_B.shape[0], S_B.shape[1]))
+print()
+
+
+
+### 011 select linear discriminants for the new feature subspace
+print('### 011')
+# eigenvalues and eigenvectors
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:,i]) for i in range(len(eigen_vals))]
+eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True)
+print('Eigenvalues in decreasing order:\n')
+for eigen_val in eigen_pairs:
+	print(eigen_val[0])
+print()
+
+# plot the linear discriminants
+tot = sum(eigen_vals.real)
+discr = [(i / tot) for i in sorted(eigen_vals.real, reverse=True)]
+cum_discr = np.cumsum(discr)
+plt.bar(range(1, 14), discr, alpha=0.5, align='center', label='individual "discriminability"')
+plt.step(range(1, 14), cum_discr, where='mid', label='cumulative "discriminability"')
+plt.ylabel('"discriminability" ratio')
+plt.xlabel('Linear Discriminants')
+plt.ylim([-0.1, 1.1])
+plt.legend(loc='best')
+plt.show()
+
+# create the transformation matrix
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis].real, eigen_pairs[1][1][:, np.newaxis].real)) # take first 2 arrays
+print('Matrix W:\n', w)
+print()
+
+
+
+### 012 project samples onto the new feature space
+X_train_lda = X_train_std.dot(w)
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+for l, c, m in zip(np.unique(y_train), colors, markers):
+	plt.scatter(X_train_lda[y_train==l, 0], X_train_lda[y_train==l, 1], c=c, label=l, marker=m)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='best')
+plt.show()
+
+
+
+### 013 LDA via scikit-learn
+#from sklearn.lda import LDA # "ImportError: No module named 'sklearn.lda'"
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+lda = LDA(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+
+# logistic regression
+lr = LogisticRegression()
+lr = lr.fit(X_train_lda, y_train)
+plot_decision_regions(X_train_lda, y_train, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc="lower left")
+plt.show()
+
+# lower the regularization strength, we can get a perfect accuracy score
+X_test_lda = lda.transform(X_test_std)
+plot_decision_regions(X_test_lda, y_test, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower left')
+plt.show()
+
+
+
+### 014 seperate half-moon shapes
+
+# create dataset
+from sklearn.datasets import make_moons
+
+X, y = make_moons(n_samples=100, random_state=123)
+plt.scatter(X[y==0, 0], X[y==0, 1],
+color='red', marker='^', alpha=0.5)
+plt.scatter(X[y==1, 0], X[y==1, 1],
+color='blue', marker='o', alpha=0.5)
+plt.show()
+
+# standard PCA
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+fig, ax = plt.subplots(nrows=1,ncols=2, figsize=(7,3))
+ax[0].scatter(X_spca[y==0, 0], X_spca[y==0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y==1, 0], X_spca[y==1, 1], color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_spca[y==0, 0], np.zeros((50,1))+0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y==1, 0], np.zeros((50,1))-0.02, color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+plt.show()
+
+# kernel pca
+from matplotlib.ticker import FormatStrFormatter
+from rbf_kernel_pca import rbf_kernel_pca
+
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+fig, ax = plt.subplots(nrows=1,ncols=2, figsize=(7,3))
+ax[0].scatter(X_kpca[y==0, 0], X_kpca[y==0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y==1, 0], X_kpca[y==1, 1], color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_kpca[y==0, 0], np.zeros((50,1))+0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y==1, 0], np.zeros((50,1))-0.02, color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.2f'))
+ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+plt.show()
