d96e4ed88d3f2a26b3ebabce4adb69aa2d90199b
YUAN Yu <yuan@archos.com>
2018-05-22 10:27:41 +0800

    chapter5 finished

--- chapter5/chapter5.py
+++ chapter5/chapter5.py
@@ -315,7 +315,7 @@ plt.show()
 
 
 
-### 016
+### 016 project new data points
 print('### 016')
 X, y = make_moons(n_samples=100, random_state=123)
 alphas, lambdas = rbf_kernel_pca(X, gamma=15, n_components=1)
@@ -333,10 +333,26 @@ def project_x(x_new, X, gamma, alphas, lambdas):
 
 x_reproj = project_x(x_new, X, gamma=15, alphas=alphas, lambdas=lambdas)
 print(x_reproj)
+print()
 
 plt.scatter(alphas[y==0, 0], np.zeros((50)), color='red', marker='^',alpha=0.5)
 plt.scatter(alphas[y==1, 0], np.zeros((50)), color='blue', marker='o', alpha=0.5)
 plt.scatter(x_proj, 0, color='black', label='original projection of point X[25]', marker='^', s=100)
 plt.scatter(x_reproj, 0, color='green', label='remapped point X[25]', marker='x', s=500)
 plt.legend(scatterpoints=1)
+plt.show()
+
+
+
+### 017 kernel PCA in scilit-learn
+from sklearn.decomposition import KernelPCA
+
+X, y = make_moons(n_samples=100, random_state=123)
+scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=15)
+X_skernpca = scikit_kpca.fit_transform(X)
+
+plt.scatter(X_skernpca[y==0, 0], X_skernpca[y==0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X_skernpca[y==1, 0], X_skernpca[y==1, 1], color='blue', marker='o', alpha=0.5)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
 plt.show()
