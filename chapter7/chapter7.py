791deea27b6aa0fb614bd8b60dcf922080880209
YUAN Yu <yuan@archos.com>
2018-06-04 17:47:14 +0800

    chapter7 finished

--- chapter7/chapter7.py
+++ chapter7/chapter7.py
@@ -158,3 +158,121 @@ for params, mean_score, scores in grid.grid_scores_:
     print("%0.3f+/-%0.2f %r" % (mean_score, scores.std() / 2, params))
 print('Best parameters: %s' % grid.best_params_)
 print('Accuracy: %.2f' % grid.best_score_)
+print()
+
+
+
+### 008 Bagging
+print('### 008')
+import pandas as pd
+
+#df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+df_wine = pd.read_csv('wine.data', header=None)
+df_wine.columns = ['Class label', 'Alcohol',
+                   'Malic acid', 'Ash',
+                   'Alcalinity of ash',
+                   'Magnesium', 'Total phenols',
+                   'Flavanoids', 'Nonflavanoid phenols',
+                   'Proanthocyanins',
+                   'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines',
+                   'Proline']
+df_wine = df_wine[df_wine['Class label'] != 1]
+y = df_wine['Class label'].values
+X = df_wine[['Alcohol', 'Hue']].values
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.40, random_state=1)
+
+from sklearn.ensemble import BaggingClassifier
+
+# use an unpruned decision tree as the base classifier
+# and create an ensemble of 500 decision trees fitted on
+# different bootstrap samples of the training dataset
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=None)
+bag = BaggingClassifier(base_estimator=tree,
+                        n_estimators=500,
+                        max_samples=1.0,
+                        max_features=1.0,
+                        bootstrap=True,
+                        bootstrap_features=False,
+                        n_jobs=1,
+                        random_state=1)
+
+from sklearn.metrics import accuracy_score
+
+tree = tree.fit(X_train, y_train)
+y_train_pred = tree.predict(X_train)
+y_test_pred = tree.predict(X_test)
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print('Decision tree train/test accuracies %.3f/%.3f' % (tree_train, tree_test))
+
+bag = bag.fit(X_train, y_train)
+y_train_pred = bag.predict(X_train)
+y_test_pred = bag.predict(X_test)
+bag_train = accuracy_score(y_train, y_train_pred)
+bag_test = accuracy_score(y_test, y_test_pred)
+print('Bagging train/test accuracies %.3f/%.3f' % (bag_train, bag_test))
+
+# compare the decision regions between the decision tree and bagging classifier
+x_min = X_train[:, 0].min() - 1
+x_max = X_train[:, 0].max() + 1
+y_min = X_train[:, 1].min() - 1
+y_max = X_train[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(nrows=1, ncols=2, sharex='col', sharey='row', figsize=(8, 3))
+for idx, clf, tt in zip([0, 1], [tree, bag], ['Decision Tree', 'Bagging']):
+    clf.fit(X_train, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    axarr[idx].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx].scatter(X_train[y_train==0, 0], X_train[y_train==0, 1], c='blue', marker='^')
+    axarr[idx].scatter(X_train[y_train==1, 0], X_train[y_train==1, 1], c='red', marker='o')
+    axarr[idx].set_title(tt)
+axarr[0].set_ylabel('Alcohol', fontsize=12)
+plt.text(10.2, -1.2, s='Hue', ha='center', va='center', fontsize=12)
+plt.show()
+
+
+
+### 009 Adaptive boosting
+print()
+print('### 009')
+from sklearn.ensemble import AdaBoostClassifier
+
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=1)
+tree = tree.fit(X_train, y_train)
+y_train_pred = tree.predict(X_train)
+y_test_pred = tree.predict(X_test)
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print('Decision tree train/test accuracies %.3f/%.3f' % (tree_train, tree_test))
+
+ada = AdaBoostClassifier(base_estimator=tree, n_estimators=500, learning_rate=0.1, random_state=0)
+ada = ada.fit(X_train, y_train)
+y_train_pred = ada.predict(X_train)
+y_test_pred = ada.predict(X_test)
+ada_train = accuracy_score(y_train, y_train_pred)
+ada_test = accuracy_score(y_test, y_test_pred)
+print('AdaBoost train/test accuracies %.3f/%.3f' % (ada_train, ada_test))
+
+x_min = X_train[:, 0].min() - 1
+x_max = X_train[:, 0].max() + 1
+y_min = X_train[:, 1].min() - 1
+y_max = X_train[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(1, 2, sharex='col', sharey='row', figsize=(8, 3))
+for idx, clf, tt in zip([0, 1], [tree, ada], ['Decision Tree', 'AdaBoost']):
+    clf.fit(X_train, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    axarr[idx].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx].scatter(X_train[y_train==0, 0],
+    X_train[y_train==0, 1], c='blue', marker='^')
+    axarr[idx].scatter(X_train[y_train==1, 0], X_train[y_train==1, 1], c='red', marker='o')
+    axarr[idx].set_title(tt)
+axarr[0].set_ylabel('Alcohol', fontsize=12)
+plt.text(10.2, -1.2, s='Hue', ha='center', va='center', fontsize=12)
+plt.show()
