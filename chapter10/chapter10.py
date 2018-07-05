46f4a613e56683bc822578fed976e150fe08aeb1
YUAN Yu <yu@yuan.org>
2018-07-05 16:49:54 +0800

    chapter10 finished

--- chapter10/chapter10.py
+++ chapter10/chapter10.py
@@ -122,3 +122,207 @@ plt.xlabel('Average number of rooms [RM]')
 plt.ylabel('Price in $1000\'s [MEDV]')
 plt.legend(loc='upper left')
 plt.show()
+
+
+
+### 006 evaluate the performance of linear regression models
+print()
+print('### 006')
+from sklearn.cross_validation import train_test_split
+
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+slr = LinearRegression()
+slr.fit(X_train, y_train)
+y_train_pred = slr.predict(X_train)
+y_test_pred = slr.predict(X_test)
+
+# residual plots
+plt.scatter(y_train_pred, y_train_pred - y_train, c='blue', marker='o', label='Training data')
+plt.scatter(y_test_pred, y_test_pred - y_test, c='lightgreen', marker='s', label='Test data')
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color='red')
+plt.xlim([-10, 50])
+plt.show()
+
+# MSE, Mean Squared Error
+from sklearn.metrics import mean_squared_error
+print('MSE train: %.3f, test: %.3f' % (
+    mean_squared_error(y_train, y_train_pred),
+    mean_squared_error(y_test, y_test_pred)))
+
+# R² score
+from sklearn.metrics import r2_score
+print('R² train: %.3f, test: %.3f' %
+    (r2_score(y_train, y_train_pred),
+    r2_score(y_test, y_test_pred)))
+print()
+
+
+
+### 007 use regularized methods for regression
+from sklearn.linear_model import Ridge
+ridge = Ridge(alpha=1.0)
+
+from sklearn.linear_model import Lasso
+lasso = Lasso(alpha=1.0)
+
+from sklearn.linear_model import ElasticNet
+elastic = ElasticNet(alpha=1.0, l1_ratio=0.5)
+
+
+
+### 008 turn a linear regression model into a curve - polynomial regression
+print('### 008')
+# 1. add a second degree polynomial term
+from sklearn.preprocessing import PolynomialFeatures
+X = np.array([258.0, 270.0, 294.0,
+              320.0, 342.0, 368.0,
+              396.0, 446.0, 480.0,
+              586.0])[:, np.newaxis]
+y = np.array([236.4, 234.4, 252.8,
+              298.6, 314.2, 342.2,
+              360.8, 368.0, 391.2,
+              390.8])
+lr = LinearRegression()
+pr = LinearRegression()
+quadratic = PolynomialFeatures(degree=2)
+X_quad = quadratic.fit_transform(X)
+
+# 2. fit a simple linear regression model for comparison
+lr.fit(X, y)
+X_fit = np.arange(250,600,10)[:, np.newaxis]
+y_lin_fit = lr.predict(X_fit)
+
+# 3. fit a multiplt regression model on transformed features for polynomial regression
+pr.fit(X_quad, y)
+y_quad_fit = pr.predict(quadratic.fit_transform(X_fit))
+# plot the results
+plt.scatter(X, y, label='training points')
+plt.plot(X_fit, y_lin_fit, label='linear fit', linestyle='--')
+plt.plot(X_fit, y_quad_fit, label='quadratic fit')
+plt.legend(loc='upper left')
+plt.show()
+
+y_lin_pred = lr.predict(X)
+y_quad_pred = pr.predict(X_quad)
+print('Training MSE linear: %.3f, quadratic: %.3f' % (
+        mean_squared_error(y, y_lin_pred),
+        mean_squared_error(y, y_quad_pred)))
+print('Training R^2 linear: %.3f, quadratic: %.3f' % (
+        r2_score(y, y_lin_pred),
+        r2_score(y, y_quad_pred)))
+print()
+
+
+
+### 009 model nonlinear relationships in the housing data
+X = df[['LSTAT']].values
+y = df['MEDV'].values
+regr = LinearRegression()
+
+# create polynomial features
+quadratic = PolynomialFeatures(degree=2)
+cubic = PolynomialFeatures(degree=3)
+X_quad = quadratic.fit_transform(X)
+X_cubic = cubic.fit_transform(X)
+
+# linear fit
+X_fit = np.arange(X.min(), X.max(), 1)[:, np.newaxis]
+regr = regr.fit(X, y)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y, regr.predict(X))
+
+# quadratic fit
+regr = regr.fit(X_quad, y)
+y_quad_fit = regr.predict(quadratic.fit_transform(X_fit))
+quadratic_r2 = r2_score(y, regr.predict(X_quad))
+
+# cubic fit
+regr = regr.fit(X_cubic, y)
+y_cubic_fit = regr.predict(cubic.fit_transform(X_fit))
+cubic_r2 = r2_score(y, regr.predict(X_cubic))
+
+# plot results
+plt.scatter(X, y, label='training points', color='lightgray')
+plt.plot(X_fit, y_lin_fit, label='linear (d=1), $R^2=%.2f$'% linear_r2,
+         color='blue', lw=2, linestyle=':')
+plt.plot(X_fit, y_quad_fit, label='quadratic (d=2), $R^2=%.2f$' % quadratic_r2,
+         color='red', lw=2, linestyle='-')
+plt.plot(X_fit, y_cubic_fit, label='cubic (d=3), $R^2=%.2f$' % cubic_r2,
+         color='green', lw=2, linestyle='--')
+plt.xlabel('% lower status of the population [LSTAT]')
+plt.ylabel('Price in $1000\'s [MEDV]')
+plt.legend(loc='upper right')
+plt.show()
+
+
+
+### 010 other way of transform feature instead of polynomial features
+# transform features
+X_log = np.log(X)
+y_sqrt = np.sqrt(y)
+
+# fit features
+X_fit = np.arange(X_log.min()-1, X_log.max()+1, 1)[:, np.newaxis]
+regr = regr.fit(X_log, y_sqrt)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y_sqrt, regr.predict(X_log))
+
+# plot results
+plt.scatter(X_log, y_sqrt, label='training points', color='lightgray')
+plt.plot(X_fit, y_lin_fit, label='linear (d=1), $R^2=%.2f$' % linear_r2,
+         color='blue', lw=2)
+plt.xlabel('log(% lower status of the population [LSTAT])')
+plt.ylabel('$\sqrt{Price \; in \; \$1000\'s [MEDV]}$')
+plt.legend(loc='lower left')
+plt.show()
+
+
+
+### 011 deal with nonlinear relationships using random forests
+print('### 011')
+## decision tree regression
+from sklearn.tree import DecisionTreeRegressor
+X = df[['LSTAT']].values
+y = df['MEDV'].values
+tree = DecisionTreeRegressor(max_depth=3)
+tree.fit(X, y)
+sort_idx = X.flatten().argsort()
+lin_regplot(X[sort_idx], y[sort_idx], tree)
+plt.xlabel('% lower status of the population [LSTAT]')
+plt.ylabel('Price in $1000\'s [MEDV]')
+plt.show()
+
+## random forest regression
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
+
+from sklearn.ensemble import RandomForestRegressor
+# use the MSE criterion to grow the individual decision trees
+forest = RandomForestRegressor(n_estimators=1000, criterion='mse', random_state=1, n_jobs=-1)
+forest.fit(X_train, y_train)
+y_train_pred = forest.predict(X_train)
+y_test_pred = forest.predict(X_test)
+print('MSE train: %.3f, test: %.3f' % (
+       mean_squared_error(y_train, y_train_pred),
+       mean_squared_error(y_test, y_test_pred)))
+print('R^2 train: %.3f, test: %.3f' % (
+       r2_score(y_train, y_train_pred),
+       r2_score(y_test, y_test_pred)))
+
+# take a look at the residuals of the prediction
+plt.scatter(y_train_pred, y_train_pred - y_train, c='black',
+            marker='o', s=35, alpha=0.5, label='Training data')
+plt.scatter(y_test_pred, y_test_pred - y_test, c='lightgreen',
+            marker='s', s=35, alpha=0.7, label='Test data')
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color='red')
+plt.xlim([-10, 50])
+plt.show()
